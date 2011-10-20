21c4afb29516b6fc9e1de1a9e7994726806bafb5
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2011-10-20 22:35:53 +0200

    Initial commit.

---
+++ src/org/ktnl2/android/NinePatchExperimentsActivity.java
@@ -0,0 +1,15 @@
+package org.ktnl2.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NinePatchExperimentsActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
